bbc1c2dcf540dba747dc9e8be70b2857bda75bbc
Noboru Katayama <katayama@rs.tus.ac.jp>
2017-06-29 13:23:13 +0900

    a commit for test branch
    a

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from neural_network import NeuralNetwork
 from dqnagent import DQNAgent
 
 FLAGS = None
-#test
+#testaaaa
 def main(_):
     # set up an environment
     env = gym.make('CartPole-v1')
